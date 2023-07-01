128eb945b4427562cfb8eda1277f75d032a2991e
Sirmuelll <balogunsamuel92@gmail.com>
2023-07-01 19:28:15 +0100

    Cleaned the code

--- Restaurant Order Code.py
+++ Restaurant Order Code.py
@@ -9,16 +9,17 @@
 # import the needed modules
 import random
 import time
+
 import pyinputplus as pyip
 
 # Here is a message that welcomes the customer
 welcome = "Hello, Welcome to forks and Fingers\n"
 for x in welcome:
     print(x, end='', flush=True)
-    time.sleep(0.1)
+    time.sleep(0.09)
 
 # This is the counter that adds up the prices of everything
-a = 0
+all_price = 0
 
 
 # This generates a code for the customer's order
@@ -27,7 +28,7 @@ a = 0
 def code_generator():
     code_character = 'abcdefghijklmnopqrstuvwxyz1234567890'
     generate_code = ''
-    for code in range(8):
+    for code in range(6):
         generate_code += random.choice(code_character)
     print(f"Your order number is {generate_code}")
 
@@ -46,6 +47,7 @@ def response():
         break
 
 
+# After a user must have selected a menu fron the list, this block asks how many pieces/plates of the menu they would like to order and returns the current price by multiplying the price of that menu with the number of unit ordered
 def reorder_and_multiplier(x, meal_name):
     # After the customer chooses a meal, this line of code splits the meal name from its price so that the next line of code would be properly executed
 
@@ -57,42 +59,48 @@ def reorder_and_multiplier(x, meal_name):
     return current_pricee
 
 
-def order_food():
-    global a
+# This block of code adds the current_pricee from the above code to the general total cost "a".
+def total(own_price, menu_list):
+    global all_price
+    current_price = reorder_and_multiplier(own_price, menu_list)
+    all_price += current_price
+    response()
 
+
+# This is the main food order block
+def order_food():
     # This creates the list of the available meal or snacks
 
     menu_list = pyip.inputMenu(
-        ["Croissant - $18", "Flat Bread - $40", "Sausage - $25", "Bacon - $50", "Forest Ham - $15"], numbered=True, prompt="Here is our menu list:\n")
+        ["Croissant - $18", "Flat Bread - $40", "Sausage - $25", "Bacon - $50", "Forest Ham - $15"], numbered=True,
+        prompt="Here is our menu list:\n")
 
     if menu_list == "Croissant - $18":
         own_price = 18
-        current_price = reorder_and_multiplier(own_price, menu_list)
-        a += current_price
-        response()
+        total(own_price, menu_list)
+
     elif menu_list == "Flat Bread - $40":
         own_price = 40
-        current_price = reorder_and_multiplier(own_price, menu_list)
-        a += current_price
-        response()
+        total(own_price, menu_list)
+
     elif menu_list == "Sausage - $25":
         own_price = 25
-        current_price = reorder_and_multiplier(own_price, menu_list)
-        a += current_price
-        response()
+        total(own_price, menu_list)
+
     elif menu_list == "Bacon - $50":
         own_price = 50
-        current_price = reorder_and_multiplier(own_price, menu_list)
-        a += current_price
-        response()
+        total(own_price, menu_list)
+
     else:
         own_price = 15
-        current_price = reorder_and_multiplier(own_price, menu_list)
-        a += current_price
-        response()
+        total(own_price, menu_list)
 
 
-order_food()
+def main():
+    order_food()
+    code_generator()
+    print(f"Your total order costs ${all_price}")
 
-code_generator()
-print(f"Your total order costs ${a}")
+
+if __name__ == "__main__":
+    main()
